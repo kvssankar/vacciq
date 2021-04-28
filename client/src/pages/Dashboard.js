49a332a48c87bfb8aa699407b91e4ae655be6c4a
Srezzx <srieshimportant@gmail.com>
2021-04-28 12:50:32 +0530

    bootstrap cdn added in index.html

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import { useSelector } from "react-redux";
 import QueueNumber from "../components/Dashboard/QueueNumber";
 import QueueTable from "../components/Dashboard/QueueTable";
 import Navbar from "../components/Dashboard/Navbar";
