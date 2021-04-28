e5455b2019fd774f676b3f4ec9806c209654259a
Srezzx <srieshimportant@gmail.com>
2021-04-29 04:30:32 +0530

    clock added

--- client/src/components/Dashboard/Navbar.js
+++ client/src/components/Dashboard/Navbar.js
@@ -1,29 +1,52 @@
-import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown, DropdownToggle, DropdownItem, NavbarText, DropdownMenu
+} from "reactstrap";
 
 const Example = (props) => {
-  const [collapsed, setCollapsed] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNavbar = () => setCollapsed(!collapsed);
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div>
-      <Navbar color="faded" light>
-        <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-        <Collapse isOpen={!collapsed} navbar>
-          <Nav navbar>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
             <NavItem>
               <NavLink href="/components/">Components</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
             </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
           </Nav>
+          <NavbarText>Simple Text</NavbarText>
         </Collapse>
       </Navbar>
     </div>
   );
-}
+};
 
 export default Example;
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,15 +2,14 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import QueueNumber from "../components/Dashboard/QueueNumber";
 import QueueTable from "../components/Dashboard/QueueTable";
-import Example from "../components/Dashboard/Navbar";
+import Navbar from "../components/Dashboard/Navbar";
 import { Alert } from "reactstrap";
 
 const Dashboard = ({}) => {
   return (
     <div>
-      <Alert color="primary">This is a primary alert — check it out!</Alert>
+      <Navbar />
       <QueueNumber />
-      <Example />
       <QueueTable />
     </div>
   );
