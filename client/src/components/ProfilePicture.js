c1dceca7a71e21dd355a5aa29f5d57f386d85bc7
Vinamra Khoria <Vinamrakhoria@gmail.com>
2021-04-30 00:03:35 +0530

    profile pic showing now

--- client/src/components/ProfilePicture.js
+++ client/src/components/ProfilePicture.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import img from './profile.png'
 
 export const ProfilePicture = (props) => {
     return (
         <div>
             <div className = "App">
-                <img class = "ProfilePicture" style = {{width: "150px", height: "150px", borderRadius : "75px"}} src = 'profile.png'/>
+                <img class = "ProfilePicture" style = {{width: "150px", height: "150px", borderRadius : "75px"}} src = {img}/>
                 <p class = "ProfileName">{props.data.name}</p>
                 <p class= "ProfileEmail text-muted" >{props.data.email}</p>
             </div>
