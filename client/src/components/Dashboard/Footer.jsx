2b69413b902731c0f600136ae46e36bb74c1b981
kvssankar <kvs.sankar001@gmail.com>
2021-05-01 08:11:00 +0530

    nav bar done

--- client/src/components/Dashboard/Footer.jsx
+++ client/src/components/Dashboard/Footer.jsx
@@ -27,9 +27,9 @@ const Example = (props) => {
     <Container className="themed-container" fluid={true}>
        <div className="footer">
           <div className="d-flex justify-content-between">
-            <div className="p-2 col-example text-left"><a href="https://www.google.com"><img src="/imgs/footer-1.png"></img></a></div>
-            <div className="p-2 col-example text-left"><img src="/imgs/footer-2.png" alt="Profile"></img></div>
-            <div className="p-2 col-example text-left"><img src="/imgs/footer-3.png" alt="Profile"></img></div>
+            <div className="p-3 col-example text-left"><a href="https://www.google.com"><img src="/imgs/footer-1.png"></img></a></div>
+            <div className="p-3 col-example text-left"><img src="/imgs/footer-2.png" alt="Profile"></img></div>
+            <div className="p-3 col-example text-left"><img src="/imgs/footer-3.png" alt="Profile"></img></div>
           </div>
         </div>
     </Container>
