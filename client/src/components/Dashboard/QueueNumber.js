15fd56813e005aa234e9a371ac63ed001cbc63ac
Srezzx <srieshimportant@gmail.com>
2021-05-01 15:47:26 +0530

    minor changes - revert if neeeded

--- client/src/components/Dashboard/QueueNumber.js
+++ client/src/components/Dashboard/QueueNumber.js
@@ -5,10 +5,10 @@ import QueueTable from "./QueueTable";
 
 const QueueNumber = (props) => {
   return (
-    <div className=" row d-flex justify-content-between queuenumbercontainer">
+    <div className="mt-5  row d-flex justify-content-between queuenumbercontainer">
       <div className="p-2 col-example">
         <div className="texts ">
-          <h6 className="fontstyle1small pb-5 mt-3">
+          <h6 className="ml-4 fontstyle1small pb-5 mt-3">
             Waiting in Queue,<br></br>Anywhere and Anytime
           </h6>
         </div>
