8e0ccae89584bb8b58676f86e65b8ff83b814bee
kvssankar <kvs.sankar001@gmail.com>
2021-05-04 10:49:23 +0530

    production

--- server/index.js
+++ server/index.js
@@ -55,9 +55,11 @@ app.use("/api/q", require("./routes/queue"));
 // app.use("/api/mail", require("./routes/mail"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve("..", __dirname, "client", "build", "index.html")
+    );
   });
 }
 
