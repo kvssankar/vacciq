6d8fb15ee225acc81d55414d97afcb2f3d9382df
kvssankar <kvs.sankar001@gmail.com>
2021-05-10 16:45:17 +0530

    imgs replace

--- client/src/components/Dashboard/Footer.jsx
+++ client/src/components/Dashboard/Footer.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import {Container} from "reactstrap";
 import "./Footer.css";
 import logout from "../../img/logout.svg"
+import home from "../../img/home.svg"
+import dashboard from "../../img/square.svg"
 import { useHistory } from "react-router";
 const Example = () => {
 const history=useHistory()
@@ -11,8 +13,8 @@ const history=useHistory()
     <Container className="themed-container" fluid={true}>
        <div className="footer">
           <div className="d-flex justify-content-between">
-            <div className="p-3 col-example text-left"><a href="/dashboard"><img src="/imgs/footer-1.png" alt="loading..."></img></a></div>
-            <div className="p-3 col-example text-left"><a href="/userdashboard"><img src="/imgs/footer-2.png" alt="Profile"></img></a></div>
+            <div className="p-3 col-example text-left"><a href="/dashboard"><img src={dashboard} alt="loading..."></img></a></div>
+            <div className="p-3 col-example text-left"><a href="/userdashboard"><img src={home} alt="Profile"></img></a></div>
             <div className="p-3 col-example text-left"><a href="#" onClick={()=>{localStorage.removeItem("state");history.push("/")}}><img src={logout} alt="Profile"></img></a></div>
           </div>
         </div>
