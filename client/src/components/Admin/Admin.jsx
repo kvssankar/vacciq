195b9db319deaacf3b32b9e841c7fa7428240dc0
kvssankar <kvs.sankar001@gmail.com>
2021-05-21 14:38:39 +0530

    done share button

--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import QRCode from "qrcode.react";
+import domtoimage from "dom-to-image";
 import "./Admin.css";
 import { useHistory } from "react-router";
 
 const Admin = ({user,center}) => {
   const history=useHistory();
+  const share = () => {
+    domtoimage.toBlob(document.querySelector("canvas")).then(function (blob) {
+      const file = new File([blob], "vacciq.png", blob);
+      if (navigator.share) {
+        navigator.share({
+          files: [file],
+          title: `Join the ${center.name} using VacciQ`,
+          text:
+            "Scan the qr code to join the queue or follow the link bellow",
+          url: `https://vacciq.herokuapp.com/quicklogin/${center._id}`,
+        });
+      } else {
+        alert("this feature is only compactable with mobiles");
+      }
+    });
+  };
     return (
       <div className="signuppagecontainer container">
         <div className="row d-flex ml-3 mt-3 ">
           <img src="/imgs/back.png" onClick={()=>history.push("/userdashboard")} alt="loading..."></img>
         </div>
-        <div className="mt-5 row justify-content-center d-flex">
+        <div className="mt-2 row justify-content-center d-flex" style={{flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
         <QRCode value={center._id}/>
+        <button style={{padding:"5px 10px"}} onClick={share} className="primary-button">Share ↱</button>
         </div>
         <div className="d-flex justify-content-between row">
             <div className="rectanglebackground2 flex-fill mt-5 p-2 col-example ">
