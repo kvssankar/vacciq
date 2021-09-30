ae135a7d3dc3fc4bb62504288af61ade5b6861bc
Srezzx <srieshimportant@gmail.com>
2021-09-30 23:20:10 +0530

    fixed some spacing issues

--- client/src/components/Dashboard/Footer.jsx
+++ client/src/components/Dashboard/Footer.jsx
@@ -3,20 +3,20 @@ import { Container } from "reactstrap";
 import "./Footer.css";
 import logout from "../../img/logout.svg";
 import home from "../../img/home.svg";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router";
 import { getq } from "../../actions/queueActions";
 
 const Example = () => {
   const history = useHistory();
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
   const [qno, setQno] = useState(0);
   const queue = useSelector((state) => state.userReducer.queue);
   const user = useSelector((state) => state.userReducer.user);
-  useEffect(()=>{
-    async function temp(){
-      await dispatch(getq(user.queue_id || user.center_id,()=>(null)));
-      if(queue){
+  useEffect(() => {
+    async function temp() {
+      await dispatch(getq(user.queue_id || user.center_id, () => null));
+      if (queue) {
         for (var i = 0; i < queue.line.length; i++) {
           setQno(i + 1);
           if (queue.line[i].user._id === user._id) {
@@ -26,22 +26,26 @@ const Example = () => {
       }
     }
     temp();
-  },[])
+  }, []);
   return (
     <Container className="themed-container" fluid={true}>
       <div className="footer" style={{ zIndex: 111 }}>
         <div className="row d-flex justify-content-between align-items-center">
           <div className="col-4">
-            {queue ? <a
-              href={user.queue_id ? "/dashboard" : "/admin"}
-              className=" qnodisplay btn btn-outline-primary"
-            >
-              QNO: {qno}
-            </a> :  <img
-            className="barcodeimg"
-            src="/imgs/barcode.png"
-            alt="barcodeScanner"
-          ></img> }
+            {queue ? (
+              <a
+                href={user.queue_id ? "/dashboard" : "/admin"}
+                className=" qnodisplay btn btn-outline-primary"
+              >
+                QNO: {qno}
+              </a>
+            ) : (
+              <img
+                className="barcodeimg"
+                src="/imgs/barcode.png"
+                alt="barcodeScanner"
+              ></img>
+            )}
           </div>
           <div style={{ marginLeft: "auto auto" }} className="col-4">
             {" "}
@@ -67,8 +71,8 @@ const Example = () => {
                 border: "none",
                 backgroundColor: "white",
               }}
-              onClick={async() => {
-                dispatch({type:"LOGOUT"});
+              onClick={async () => {
+                dispatch({ type: "LOGOUT" });
                 history.push("/");
               }}
             >
